9e3c3a38402072338d9efa6f101095ea0472a10b
vickichowder <vechow12@gmail.com>
2016-02-19 11:21:23 -0500

    Multithreading for pandas to_sql

---
+++ toSql.py
@@ -0,0 +1,26 @@
+import threading
+
+def tosql(df, *args, **kargs):
+    CHUNKSIZE = 1000
+    INITIAL_CHUNK = 100
+    if len(df) <= CHUNKSIZE:
+        df.to_sql(*args, **kargs)
+        print('added: smaller than or same as chunksize', len(df), args[0])
+        return
+    if len(df) > CHUNKSIZE:
+        df.iloc[:INITIAL_CHUNK, :].to_sql(*args, **kargs)
+    if kargs['if_exists'] == 'replace':
+        kargs['if_exists'] = 'append'
+    workers = []
+    for x in range((len(df) - INITIAL_CHUNK)//CHUNKSIZE):
+        t = threading.Thread(target=lambda: df.iloc[INITIAL_CHUNK+x*CHUNKSIZE:INITIAL_CHUNK+(x+1)*CHUNKSIZE, :].to_sql(*args, **kargs))
+        t.start()
+        workers.append(t)
+    try:
+        print('total number of threads:', x, 'for', args[0])
+        df.iloc[INITIAL_CHUNK+(x+1)*CHUNKSIZE:, :].to_sql(*args, **kargs)
+    except UnboundLocalError as e:
+        print('-- unbound local error --', e)
+        pass
+    [t.join() for t in workers]
+    print('added data:', len(df), 'to', args[0])
